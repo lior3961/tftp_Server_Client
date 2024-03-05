7799630210df18520e43f1e08ca7a517420b7c64
roy houber <royhou@post.bgu.ac.il>
2024-03-05 19:28:50 +0200

    EncDec finished

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
@@ -1,21 +1,47 @@
 package bgu.spl.net.impl.tftp;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 import bgu.spl.net.api.MessageEncoderDecoder;
 
 public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
-    //TODO: Implement here the TFTP encoder and decoder
 
+    private byte[] bytes = new byte[1 << 10]; //start with 1k
+    private int len = 0;
 
     @Override
-    public byte[] decodeNextByte(byte nextByte) {
-        // TODO: implement this
+    public byte[] decodeNextByte(byte nextByte)
+    {
+        if (nextByte == 0) {
+            return popBytes();
+        }
 
+        pushByte(nextByte);          
         return null;
     }
 
+    private void pushByte(byte nextByte)
+    {
+        if (len >= bytes.length) {
+            bytes = Arrays.copyOf(bytes, len * 2);
+        }
+        bytes[len++] = nextByte;
+    }
+
     @Override
-    public byte[] encode(byte[] message) {
-        //TODO: implement this
-        return null;
+    public byte[] encode(byte[] message)
+    {
+        byte[] ans = Arrays.copyOf(message , message.length+1);
+        ans[ans.length-1] = 0;
+
+        return  ans;
+    }
+
+    private byte[] popBytes()
+    {
+        byte[] result = Arrays.copyOf(bytes, len);   
+        len = 0;
+        return result;
     }
 }
