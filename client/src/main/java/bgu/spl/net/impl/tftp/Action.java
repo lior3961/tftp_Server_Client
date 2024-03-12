e68341a619da8c891d7036a4447fdb7070b5cbfb
roy houber <royhou@post.bgu.ac.il>
2024-03-12 21:07:36 +0200

    day one client

---
+++ client/src/main/java/bgu/spl/net/impl/tftp/Action.java
@@ -0,0 +1,330 @@
+package bgu.spl.net.impl.tftp;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Vector;
+public class Action {
+
+
+    private boolean needToSendData;
+    private boolean needToDeleteFile;
+    private boolean needToDisc;
+    private Vector<String> clientFiles;
+    private String fileName;
+    private String userName;
+    private boolean connected;
+    
+
+    public Action(boolean connected)
+    {
+        this.needToSendData = false;
+        this.needToDeleteFile = false;
+        Path serverFilesFolderPath = (Paths.get("").toAbsolutePath()).resolve("Files");//get "Files" path
+        this.clientFiles = getFileList(serverFilesFolderPath.toString());
+        this.fileName = "";
+        this.userName = "";
+        this.connected = connected;
+        this.needToDisc = false;
+    }
+
+    public byte[] createAction(String input)
+    {
+        byte[] msg = null;
+        short opCode;
+        int spaceIndex = input.indexOf(" ");
+        if(spaceIndex != -1)
+        {
+            String action = input.substring(0,spaceIndex);
+            String content = input.substring(spaceIndex+1);
+            switch (action) 
+            {
+                case "LOGRQ":
+                    this.userName = content;
+                    opCode = 7;
+                    this.needToSendData = false;
+                    this.needToDeleteFile = false;
+                    this.needToDisc = false;
+                    msg = createPacket(opCode , content);
+                    break;
+                case "RRQ":
+                    this.fileName = content;
+                    if(!this.clientFiles.contains(content))
+                    {
+                        opCode = 1;
+                        this.needToSendData = false;
+                        this.needToDeleteFile = true;
+                        this.needToDisc = false;
+                        msg = createPacket(opCode , content);
+                        updateFiles(content, false, null);
+                        break;
+                    }
+                    System.out.println("File already exist");
+                    break;
+                case "WRQ":
+                    this.fileName = content;
+                    if(this.clientFiles.contains(content))
+                    {
+                        opCode = 2;
+                        this.needToSendData = true;
+                        this.needToDeleteFile = false;
+                        this.needToDisc = false;
+                        msg = createPacket(opCode , content);
+                    }
+                    System.out.println("File doesnt exist");
+                    break;
+                case "DELRQ":
+                    this.fileName = content;
+                    opCode = 8;
+                    this.needToSendData = false;
+                    this.needToDeleteFile = false;
+                    this.needToDisc = false;
+                    msg = createPacket(opCode , content);
+                    break;
+            }
+        }
+        else
+        {
+            switch (input) 
+            {
+                case "DISC":
+                    opCode = 10;
+                    this.needToSendData = false;
+                    this.needToDeleteFile = false;
+                    this.needToDisc = true;
+                    msg = createPacket(opCode , input);
+                    break;
+                case "DIRQ":
+                    opCode = 6;
+                    this.needToSendData = false;
+                    this.needToDeleteFile = false;
+                    this.needToDisc = false;
+                    msg = createPacket(opCode , input);
+                    break;
+            }
+        }
+        return msg;
+    }
+
+    public byte[] createPacket(short opCode, String content)
+    {
+        byte lastbyte = 0;
+        byte[] result = null;
+        int offset = 0;
+        byte[] opCodeBytes;
+        byte[] contentByts;
+        switch (opCode) {
+            case 1:
+                result = new byte[2+content.length()+1];
+                opCodeBytes = shortToArray(opCode);
+                contentByts = content.getBytes(StandardCharsets.UTF_8);
+                System.arraycopy(opCodeBytes, 0, result, offset, opCodeBytes.length);
+                offset += opCodeBytes.length;
+                System.arraycopy(contentByts, 0, result, offset, contentByts.length);
+                result[result.length-1] = lastbyte;
+                break;
+            case 2:
+                result = new byte[2+content.length()+1];
+                opCodeBytes = shortToArray(opCode);
+                contentByts = content.getBytes(StandardCharsets.UTF_8);
+                System.arraycopy(opCodeBytes, 0, result, offset, opCodeBytes.length);
+                offset += opCodeBytes.length;
+                System.arraycopy(contentByts, 0, result, offset, contentByts.length);
+                result[result.length-1] = lastbyte;
+                break;
+            case 6:
+                opCodeBytes = shortToArray(opCode);
+                result = Arrays.copyOf(opCodeBytes, opCodeBytes.length);
+                break;
+            case 7:
+                result = new byte[2+content.length()+1];
+                opCodeBytes = shortToArray(opCode);
+                contentByts = content.getBytes(StandardCharsets.UTF_8);
+                System.arraycopy(opCodeBytes, 0, result, offset, opCodeBytes.length);
+                offset += opCodeBytes.length;
+                System.arraycopy(contentByts, 0, result, offset, contentByts.length);
+                result[result.length-1] = lastbyte;
+                break;
+            case 8:
+                result = new byte[2+content.length()+1];
+                opCodeBytes = shortToArray(opCode);
+                contentByts = content.getBytes(StandardCharsets.UTF_8);
+                System.arraycopy(opCodeBytes, 0, result, offset, opCodeBytes.length);
+                offset += opCodeBytes.length;
+                System.arraycopy(contentByts, 0, result, offset, contentByts.length);
+                result[result.length-1] = lastbyte;
+                break;
+            case 10:
+                opCodeBytes = shortToArray(opCode);
+                result = Arrays.copyOf(opCodeBytes, opCodeBytes.length);
+                break;            
+        }
+        offset = 0;
+        return result;
+    }
+
+    public byte[] gotMessageFromServer(byte[] packet)
+    {
+        if(packet == null)
+        {
+            return null;
+        }
+        short opCode = arrayToShort(packet);
+        byte[] blockNumberBytes = new byte[2];
+        short blockNumber;
+        byte[] result = null;
+        switch (opCode)
+        {
+            case 3:
+                blockNumberBytes[0] = packet[2];
+                blockNumberBytes[1] = packet[3];
+                blockNumber = arrayToShort(blockNumberBytes);
+                result = createAckPacket(blockNumber);
+                byte[] data = Arrays.copyOfRange(packet, 6, packet.length);
+                updateFiles(this.fileName, false, data);
+                break;
+            case 4:
+                if(needToDisc)
+                {
+                    this.connected = false;
+                }
+                if(needToSendData)
+                {
+                    blockNumberBytes[0] = packet[2];
+                    blockNumberBytes[1] = packet[3];
+                    blockNumber = arrayToShort(blockNumberBytes);
+                    System.out.println("ACK " + blockNumber);
+                    blockNumber += 1;
+                    result = createDataPacket(result,blockNumber);
+                    break;
+                }
+                System.out.println("ACK 0");
+                break;
+            case 5:
+            {
+                byte[] errorMsgBytes = Arrays.copyOfRange(packet, 4, packet.length-1);
+                String decodedString = new String(errorMsgBytes, StandardCharsets.UTF_8);
+                System.out.println(decodedString);
+                if(this.needToDeleteFile)
+                {    
+                    updateFiles(this.fileName , true , null);
+                }
+            }
+            case 9:
+            {
+                byte[] fileNameBytes = Arrays.copyOfRange(packet, 3, packet.length-1);
+                String fileName = new String(fileNameBytes, StandardCharsets.UTF_8);
+                byte deleteOrAdded = packet[2];
+                if(deleteOrAdded == 0)
+                {
+                    System.out.println("BCAST del " + fileName);
+                }
+                else
+                {
+                    System.out.println("BCAST add " + fileName);
+                }
+                break;
+            }
+        }
+        return result;
+    }
+
+    public byte[] shortToArray(short num)
+    {
+        byte [] result = new byte []{( byte )( num >> 8) , ( byte ) ( num & 0xff ) };
+        return result;
+
+    }
+
+    public short arrayToShort(byte[] arr)
+    {
+        short b_short = (short) (((short) arr[0]) << 8 | (short) (arr[1]) & 0x00ff);
+        return b_short;
+    }
+
+    public byte[] createDataPacket(byte[] content,short blockNumber)
+    {
+        byte [] result = new byte[6+content.length];
+        byte[] opCodeBytes = shortToArray((short)3);
+        byte[] dataSizeBytes = shortToArray((short)content.length);
+        byte[] blockNumberBytes = shortToArray(blockNumber);
+        int offset = 0;
+        System.arraycopy(opCodeBytes, 0, result, offset, opCodeBytes.length);
+        offset += opCodeBytes.length;
+        System.arraycopy(dataSizeBytes, 0, result, offset, dataSizeBytes.length);
+        offset += dataSizeBytes.length;
+        System.arraycopy(blockNumberBytes, 0, result, offset, blockNumberBytes.length);
+        offset += blockNumberBytes.length;
+        System.arraycopy(content, 0, result, offset, content.length);
+        return result;
+    }
+
+    public byte[] createAckPacket(short blockNumber)
+    {
+        short ackCode = 4;
+        byte [] opCodeBytes = shortToArray(ackCode);
+        byte [] blockNumberBytes = shortToArray(blockNumber);
+        byte[] result = Arrays.copyOf(opCodeBytes, opCodeBytes.length+blockNumberBytes.length);
+        System.arraycopy(blockNumberBytes, 0, result, opCodeBytes.length, blockNumberBytes.length);
+        return result;
+    }
+
+    public void updateFiles(String fileName, boolean delete, byte[] data)
+    {
+        Path serverFilesFolderPath = (Paths.get("").toAbsolutePath()).resolve("Files");
+        Path filePath = serverFilesFolderPath.resolve(fileName);
+        if(delete)
+        {
+            this.clientFiles.remove(fileName);
+            File file = filePath.toFile();
+            file.delete();
+        }
+        else
+        {
+            if(!this.clientFiles.contains(fileName))
+            {
+                this.clientFiles.add(fileName);
+                System.out.println();
+            }
+            try 
+            {
+                FileOutputStream fos = new FileOutputStream(fileName.toString(),true); //new file output stream with file path
+                if(data != null)
+                {
+                    fos.write(data); //creating file
+                    fos.close();
+                }
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+            
+        }
+    }
+
+    public boolean shouldTerminate()
+    {
+        return !this.connected;
+    }
+    
+    public static Vector<String> getFileList(String directoryPath) {
+    Vector<String> fileList = new Vector<String>();
+    File directory = new File(directoryPath);      
+    File[] files = directory.listFiles();
+    if (files != null)
+    {
+        for (File file : files) {
+            if (file.isFile()) {
+                fileList.add(file.getName());
+            }
+        }
+    }       
+    return fileList;
+    }
+
+}
--- client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
@@ -1,7 +1,79 @@
 package bgu.spl.net.impl.tftp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class TftpClient {
-    //TODO: implement the main logic of the client, when using a thread per client the main logic goes here
+
+    public static Socket keyBoardSocket;
+    public static boolean needTolisten = false;
+    public static Scanner scanner = new Scanner(System.in);
+    public static boolean connected = true;
     public static void main(String[] args) {
-        System.out.println("implement me!");
+        Action actions = new Action(connected);
+        Thread keyBoardHandler = new Thread( ()-> {
+            try (Socket sock = new Socket(args[0], 7777);
+                BufferedOutputStream out = new BufferedOutputStream(sock.getOutputStream()))
+            {
+                byte[] message;
+                keyBoardSocket = sock;
+                while(!actions.shouldTerminate())
+                {
+                    if(!needTolisten)
+                    {
+                        String input = scanner.nextLine();
+                        message = actions.createAction(input);
+                        if(message != null)
+                        {
+                            out.write(message);
+                            out.flush();
+                            needTolisten = true;
+                        }
+                    }
+                }
+                keyBoardSocket.close();
+            }
+            catch(IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        });
+       
+        Thread listening = new Thread( ()->
+        {
+            try (Socket sock = new Socket(args[0], 7777);
+            BufferedInputStream in = new BufferedInputStream(sock.getInputStream());
+            BufferedOutputStream out = new BufferedOutputStream(sock.getOutputStream()))
+            {
+                int read;
+                while(!actions.shouldTerminate())
+                {
+                    int packetSize = in.available();
+                    if(packetSize > 0)
+                    {
+                        byte[] inputPacket = new byte[packetSize];
+                        int bytesRead = in.read(inputPacket);
+                        byte[] returnMessage = actions.gotMessageFromServer(inputPacket);
+                        out.write(returnMessage);
+                        out.flush();
+                    }
+                }
+                
+            }
+            catch(IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        });
+        
+        keyBoardHandler.start();
+        listening.start();
+        
     }
 }
+
